1a1a2c15f01511caf7c91c890335b8dadae005b6
scott <dev.scott.kim@gmail.com>
2025-06-18 11:21:21 +0900

    add regist page & button

---
+++ lib/features/login/regist_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RegistPage extends StatelessWidget {
+  final TextEditingController nickNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  RegistPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('로그인'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome Back!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 32),
+
+            // 닉네임 입력 필드
+            TextField(
+              controller: nickNameController,
+              decoration: const InputDecoration(
+                labelText: '닉네임',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 16),
+
+            // 이메일 입력 필드
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: '이메일',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 16),
+
+            // 비밀번호 입력 필드
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(
+                labelText: '비밀번호',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 24),
+
+            // 로그인 버튼
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  // 로그인 로직 처리
+                  final email = emailController.text;
+                  final password = passwordController.text;
+                  print('로그인 시도: $email / $password');
+                },
+                child: const Text('로그인'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
